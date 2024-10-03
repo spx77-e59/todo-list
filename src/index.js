cfaeee2374e383df10bbcc54dccb64efd4f2f934
somtsho <179671017+spx77-e59@users.noreply.github.com>
2024-10-03 22:09:56 +0600

    Complete header styling

--- src/index.js
+++ src/index.js
@@ -7,9 +7,8 @@ import showProjectsAndList from "./display/showProjectsAndList.js";
 import showProjects from "./display/showProjects.js";
 
 const homeBtn = document.querySelector("#home-btn");
-
 const projectsBtn = document.querySelector("#projects-btn");
-
+const navElement = document.querySelector("nav");
 const defaultProject = new Project(generateUniqueID(), "", []);
 
 Storage.Projects.push(defaultProject);
@@ -23,4 +22,16 @@ homeBtn.addEventListener("click", () => {
 
 projectsBtn.addEventListener("click", () => {
   showProjects();
+});
+
+homeBtn.classList.add("activeState");
+navElement.addEventListener("click", (e) => {
+  const target = e.target;
+  if (target.tagName === "BUTTON") {
+    const buttons = navElement.querySelectorAll("button");
+    buttons.forEach((button) => {
+      button.classList.remove("activeState");
+    });
+    target.classList.add("activeState");
+  }
 });
